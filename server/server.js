a95c771890eb0e684051d77252e59bd79dd6dea0
Harshini Kosuri <harshini.kosuri01@gmail.com>
2022-04-11 22:30:55 -0700

    checking from harshini

--- server/server.js
+++ server/server.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 
 app.listen(5000,(req,res)=>{
-    console.log("Srever running on port 8585")
+    console.log("Srever running on port 8585 by harshini")
 });
