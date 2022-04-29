01b31658a951080f1d05a6f50e3faf1de7636984
Varun Reddy <varunreddy.yedulla@sjsu.edu>
2022-04-28 18:51:33 -0700

    Added user profile and users

--- kafka/routes/userRoutes.js
+++ kafka/routes/userRoutes.js
@@ -1,19 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Validation = require('../validations/Validation');
-const UserController = require('../controllers/UserController');
+const Validation = require("../validations/Validation");
+const UserController = require("../controllers/UserController");
 
-router.post('/register', Validation.registrationValidation(), UserController.createUser)
-router.post('/login', UserController.login)
-router.get('/:userId/profile', UserController.getUserProfile)
-router.get('/:userId/profile/top_posts', UserController.getUserProfileTopPosts)
-router.get('/:userId/activity/answers', UserController.getUserAnswers)
-router.get('/:userId/activity/questions', UserController.getUserQuestions)
-router.get('/:userId/activity/bookmarks', UserController.getUserBookmarks)
-router.get('/:userId/activity/badges', UserController.getUserBadges)
-router.get('/:userId/activity/tags', UserController.getUserTags)
-router.get('/:username',UserController.getUser)
+router.post(
+  "/register",
+  Validation.registrationValidation(),
+  UserController.createUser
+);
+router.post("/login", UserController.login);
+router.get("/:userId/profile", UserController.getUserProfile);
+router.get("/:userId/profile/top_posts", UserController.getUserProfileTopPosts);
+router.get("/:userId/activity/answers", UserController.getUserAnswers);
+router.get("/:userId/activity/questions", UserController.getUserQuestions);
+router.get("/:userId/activity/bookmarks", UserController.getUserBookmarks);
+router.get("/:userId/activity/badges", UserController.getUserBadges);
+router.get("/:userId/activity/tags", UserController.getUserTags);
+router.get("/:username", UserController.getUser);
+router.get("/profile/getProfile", UserController.getProfile);
+router.get("/", UserController.getAllUsers);
 
-
-
-module.exports = router
+module.exports = router;
