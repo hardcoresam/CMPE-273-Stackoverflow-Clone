c2a61152caf97005d35a22a7ab942fb7072de2e8
unknown <akshaymadiwalar@gmail.com>
2022-04-30 22:14:50 -0700

    tag overview

--- backend/models/mysql/Tag.js
+++ backend/models/mysql/Tag.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
         name: {
             type: DataTypes.STRING,
             allowNull: false,
-            primaryKey:true
+            unique:true
         },
         description: {
             type: DataTypes.STRING
