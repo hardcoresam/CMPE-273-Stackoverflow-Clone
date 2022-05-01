aa61d83af671382b6ccada27e56e327c99f46b65
Santhosh bodla <santhosh.bodla.16cse@bml.edu.in>
2022-04-30 19:10:38 -0700

    User Activity Badges and Tags Integrated

--- client/src/components/User/BadgeList.js
+++ client/src/components/User/BadgeList.js
@@ -1,23 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Row, Col, Card, Button } from 'react-bootstrap'
 const BadgeList = (props) => {
+  const arr = [[1, 2,3], [3, 4,5]]
+  const [state,setstate] = useState([]);
+  useEffect(()=>{
+    let gridProducts =[];
+      for(let i=0;i<props.state.length;i=i+4){
+        gridProducts.push(props.state.slice(i,i+4));
+      }
+      setstate(gridProducts)
+  },[])
   return (
     <div>
       <Row>
         <h5>{props.state.length} {props.text}</h5>
       </Row>
       {
-        props.state.map((i) => (
-          <Card>
-            <div style={{ margin: "1rem" }}>
+        state.map((array) => (
+          <Row>
+            {array.map((obj) => (
+
+              <Card style={{ width: "8rem" , marginRight:"3rem", marginBottom:"1rem"}}>
               <Row>
-                <Col sm={2}><text>{i.name}</text></Col>
-                <Col></Col>
-                <Col sm={1}>{i.score}</Col>
-                <Col sm={1}>{i.totalPosts}</Col>
+              <Col><text style={{color:"gold"}}><i class="fa-solid fa-circle" style={{fontSize:"10px"}}></i></text></Col>
+              <Col><text>{obj.name}</text>
+              </Col>
+              
               </Row>
-            </div>
-          </Card>
+             
+              </Card>
+            ))}
+          </Row>
         ))
       }
     </div>
