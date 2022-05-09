bd365403e6a3d455f10a98d95042342551e10780
unknown <akshaymadiwalar@gmail.com>
2022-05-08 17:01:35 -0700

    admin dashboard graphs integration completed

--- client/src/components/Dashboard/Admin/AdminDashboard.js
+++ client/src/components/Dashboard/Admin/AdminDashboard.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Row, Col, Button, Modal } from 'react-bootstrap'
+import axios from 'axios'
+import Constants from './../../util/Constants.json'
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -19,7 +21,11 @@ ChartJS.register(
   Tooltip,
   Legend
 );
+
+
+
 const AdminDashboard = () => {
+
   const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
   const options = {
     responsive: true,
@@ -39,17 +45,16 @@ const AdminDashboard = () => {
     datasets: [
       {
         label: 'Dataset 1',
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
-      },
-      {
-        label: 'Dataset 2',
         backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        data: [20, 10],
       },
     ],
   };
+
+
   const [modal, setmodal] = useState(false);
   const [dashboardflag, setdashboardflag] = useState(true);
-  
+
   const openTagModal = () => {
     setmodal(true);
   }
@@ -61,6 +66,190 @@ const AdminDashboard = () => {
   const openpendings = () => {
     setdashboardflag(false)
   }
+
+  const [noOfQuestionsPerDay, setNoOfQuestionsPerDay] = useState({})
+  const [optionsNoOfQuestionsPerDay, setOptionsNoOfQuestionsPerDay] = useState({})
+
+  const [topTenViewedQuestions, setTopTenViewedQuestions] = useState({})
+  const [optionsTopTenViewedQuestions, setOptionsTopTenViewedQuestions] = useState({})
+
+  const [topTenTags, setTopTenTags] = useState({})
+  const [optionsTopTenTags, setOptionsTopTenTags] = useState({})
+
+  const [topTenUsers, setTopTenUsers] = useState({})
+  const [optionsTopTenUsers, setOptionsTopTenUsers] = useState({})
+
+  const [leastTenUsers, setLeastTenUsers] = useState({})
+  const [optionsLeastTenUsers, setOptionsLeastTenUsers] = useState({})
+
+
+  useEffect(() => {
+    async function adminstats() {
+      const res = await axios.get(`${Constants.uri}/api/admin/stats`, { withCredentials: true })
+      console.log(res)
+      const { data } = res
+
+      //-------------------------------No of questions per day------------------------------------------------
+      var ln = []
+      var dn = []
+      var datan = {}
+      for (var i = 0; i < data.noOfQuestionsPerDay.length; i++) {
+        ln.push(data.noOfQuestionsPerDay[i].post_created_date)
+        dn.push(data.noOfQuestionsPerDay[i].posts_count)
+      }
+      datan = {
+        labels: ln,
+        datasets: [
+          {
+            label: 'Questions Count',
+            backgroundColor: '#2992e3',
+            data: dn,
+          },
+        ]
+      }
+      setNoOfQuestionsPerDay(datan)
+      setOptionsNoOfQuestionsPerDay({
+        ...options, plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: 'No of Questions asked per day',
+          },
+        },
+      })
+
+      //--------------------------------------------Top ten viewed questions----------------------------------------
+      var ln = []
+      var dn = []
+      var datan = {}
+      for (var i = 0; i < data.topTenViewedQuestions.length; i++) {
+        ln.push(data.topTenViewedQuestions[i].id)
+        dn.push(data.topTenViewedQuestions[i].views_count)
+      }
+      datan = {
+        labels: ln,
+        datasets: [
+          {
+            label: 'Question Count',
+            backgroundColor: '#32a852',
+            data: dn,
+          },
+        ]
+      }
+      setTopTenViewedQuestions(datan)
+      setOptionsTopTenViewedQuestions({
+        ...options, plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: 'Top 10 most viewed Questions',
+          },
+        },
+      })
+
+      //--------------------------------------------Top ten Tags----------------------------------------
+      var ln = []
+      var dn = []
+      var datan = {}
+      for (var i = 0; i < data.topTenTags.length; i++) {
+        ln.push(data.topTenTags[i].name)
+        dn.push(data.topTenTags[i].no_of_questions)
+      }
+      datan = {
+        labels: ln,
+        datasets: [
+          {
+            label: 'Number of questions',
+            backgroundColor: '#a84432',
+            data: dn,
+          },
+        ]
+      }
+      setTopTenTags(datan)
+      setOptionsTopTenTags({
+        ...options, plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: 'Top 10 tags with most questions asked',
+          },
+        },
+      })
+
+
+      //--------------------------------------------Top ten users----------------------------------------
+      var ln = []
+      var dn = []
+      var datan = {}
+      for (var i = 0; i < data.topTenUsers.length; i++) {
+        ln.push(data.topTenUsers[i].username)
+        dn.push(data.topTenUsers[i].reputation)
+      }
+      datan = {
+        labels: ln,
+        datasets: [
+          {
+            label: 'Reputation Score',
+            backgroundColor: '#e3dd29',
+            data: dn,
+          },
+        ]
+      }
+      setTopTenUsers(datan)
+      setOptionsTopTenUsers({
+        ...options, plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: 'Top 10 Users with most Reputation',
+          },
+        },
+      })
+
+      //--------------------------------------------Least 10 users----------------------------------------
+      var ln = []
+      var dn = []
+      var datan = {}
+      for (var i = 0; i < data.leastTenUsers.length; i++) {
+        ln.push(data.leastTenUsers[i].username)
+        dn.push(data.leastTenUsers[i].reputation)
+      }
+      datan = {
+        labels: ln,
+        datasets: [
+          {
+            label: 'Reputation score',
+            backgroundColor: '#29e3d3',
+            data: dn,
+          },
+        ]
+      }
+      setLeastTenUsers(datan)
+      setOptionsLeastTenUsers({
+        ...options, plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: 'Least 10 Users with least Reputation',
+          },
+        },
+      })
+
+
+    }
+    adminstats()
+  }, [])
+
   return (
     <div style={{ margin: "1rem", backgroundColor: "#e6e6e6", width: "78rem", height: "78rem" }}>
       <Row style={{ margin: "1rem" }}>
@@ -84,28 +273,38 @@ const AdminDashboard = () => {
             <Col sm={1}></Col>
             <Col sm={5}>
               <div>
-              <Bar options={options} data={data} />
+                {noOfQuestionsPerDay && noOfQuestionsPerDay.datasets && noOfQuestionsPerDay.datasets[0].data && (
+                  <Bar options={optionsNoOfQuestionsPerDay} data={noOfQuestionsPerDay} />
+                )}
+
               </div>
 
             </Col>
             <Col sm={5}>
               <div>
-              <Bar options={options} data={data} />
+                {topTenViewedQuestions && topTenViewedQuestions.datasets && topTenViewedQuestions.datasets[0].data && (
+                  <Bar options={optionsTopTenViewedQuestions} data={topTenViewedQuestions} />
+                )}
               </div>
 
             </Col>
           </Row>
+          <br />
           <Row>
             <Col sm={1}></Col>
             <Col sm={5}>
               <div>
-              <Bar options={options} data={data} />
+                {topTenTags && topTenTags.datasets && topTenTags.datasets[0].data && (
+                  <Bar options={optionsTopTenTags} data={topTenTags} />
+                )}
               </div>
 
             </Col>
             <Col sm={5}>
               <div>
-              <Bar options={options} data={data} />
+                {topTenUsers && topTenUsers.datasets && topTenUsers.datasets[0].data && (
+                  <Bar options={optionsTopTenUsers} data={topTenUsers} />
+                )}
               </div>
 
             </Col>
@@ -114,11 +313,13 @@ const AdminDashboard = () => {
             <Col sm={4}></Col>
             <Col sm={5}>
               <div>
-              <Bar options={options} data={data} />
+                {leastTenUsers && leastTenUsers.datasets && leastTenUsers.datasets[0].data && (
+                  <Bar options={optionsLeastTenUsers} data={leastTenUsers} />
+                )}
               </div>
 
             </Col>
-            
+
           </Row>
 
         </div> :
