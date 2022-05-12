85568d8e54c15f3b888720640c95dacc4f974dbe
Abhiram Yenugadhati <abhiramyenugadhati@gmail.comgit>
2022-05-12 13:35:48 -0700

    Adding chats

--- client/src/components/Messages/Chat.js
+++ client/src/components/Messages/Chat.js
@@ -57,38 +57,7 @@ const Chat = () => {
     }, [sent])
 
     return (
-        <div>
-            <Row style={{marginTop:'15px', marginLeft:'15%',marginRight:'15%'}}>
-                <Col sm={2}></Col>
-                <Col>
-                    <Row>
-                        <Col sm={1}><img style={{ width: "3rem", height: "3rem", borderRadius: "3rem" }} src={img}></img></Col>
-                        <Col sm={3}><h4>{recipient}</h4></Col>
-                        
-
-                    </Row>
-                    <Row style={{ marginTop: "2rem" }}>
-                        <Card style={{ height: "25rem", width: "38rem" }}>
-                            <Row style={{ height: "23rem", width: "38rem", backgroundColor: 'black', color: 'white' }}>
-                                {messages && messages.map(message => (
-                                    <>
-                                        <Row style={{ textAlign: message.from === username ? 'left' : 'right' }}>
-                                            <span><span style={{fontWeight:'lighter'}}>{message.from}</span>&nbsp;&nbsp;{message.message} <br /><span style={{ fontStyle: 'italic', fontSize: 12, fontWeight: 'lighter' }}>{moment(message.createdAt).fromNow()}</span></span>
-                                        </Row>
-
-                                    </>
-                                ))}
-                            </Row>
-                            <Row>
-                                <Col sm={11}><input style={{ width: "34rem", height:"3rem" }} placeholder="Type a new message..." name="newMessage" value={newMessage} onChange={(e)=>onChangeMessageData(e)}></input>
-                                </Col>
-                                <Col><i style={{ cursor: "pointer" }} class="fa fa-paper-plane" aria-hidden="true" onClick={()=>sendMessage()}></i></Col>
-                            </Row>
-                        </Card>
-                    </Row>
-                </Col>
-            </Row>
-        </div>
+       
     )
 }
 
