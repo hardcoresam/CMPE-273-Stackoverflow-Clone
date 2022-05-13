ccbb46539dbfb122a4c6bd84cb6114673f11b95f
Harshini Kosuri <harshini.kosuri01@gmail.com>
2022-05-12 18:57:27 -0700

    intergrated login page

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -39,7 +39,44 @@ const Login = (props) => {
           })
     }
     return (
-        
+        <Modal
+            {...props}
+            size="sm"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Log in
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body className="signmodalbody">
+                {error && <Alert variant="danger">{error}</Alert> 
+                }
+
+                <div style={{display:"flex", flexDirection : "column", justifyContent:"space-between", height:"11rem"}}>
+                <Row style={{marginTop:"-5px"}}><label for="Email">Email</label></Row>
+                <Row style={{ margin: "1px" }}><input onChange={(e) => {
+
+                    setEmail(e.target.value)
+                }} id="Email" ></input></Row>
+
+                <Row><label for="Password">Password</label></Row>
+                <Row style={{ margin: "1px" }}><input type="password" onChange={(e) => {
+                    setPassword(e.target.value)
+                }} id="Second Name" ></input></Row>
+
+                <Row style={{ margin: "1px", marginTop:"7px" }}><Button onClick={signInUser} className="signinButton">Log in</Button></Row>
+                </div>
+                
+
+                {isLoading && <Spinner variant="primary" animation="border"/>
+                }
+
+
+            </Modal.Body>
+
+        </Modal>
     )
 }
 
